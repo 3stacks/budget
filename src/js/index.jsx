4ec5235ee2a3eb23ca65e94a1f27918b0e8ab004
Luke Boyle <luke.boyle.au@gmail.com>
2016-09-28 10:48:09 +1000

    feat: add calculation for savings

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -5,6 +5,20 @@ import savingsInput from '../components/savings-input.jsx';
 import { calculateSavingsInAYear } from './utils/savings-utils';
 import { calculateRepayments } from './utils/debt-utils';
 
+const viewState = {
+    billFormData: {
+        debt: 0,
+        rate: 0,
+        repay: 0
+    },
+    savingsFormData: {
+        savings: 0,
+        rate: 0,
+        payment: 0
+    },
+    creditRepayment: {},
+    savingsAmount: {}
+};
 
 function handleCreditCardDebtCalculation(event) {
     event.preventDefault();
@@ -12,7 +26,7 @@ function handleCreditCardDebtCalculation(event) {
     const repayment = parseInt(viewState.billFormData.repay) * 100;
     const rate = parseInt(viewState.billFormData.rate) / 100;
     viewState.creditRepayment = calculateRepayments(debt, repayment, rate);
-    const creditChart = new Chart(document.getElementById('chart'), {
+    const creditChart = new Chart(document.getElementById('credit_chart'), {
         type: 'line',
         data: {
             labels: Object.keys(viewState.creditRepayment),
@@ -31,17 +45,17 @@ function handleCreditCardDebtCalculation(event) {
 
 function handleSavingsCalculation(event) {
     event.preventDefault();
-    const debt = parseInt(viewState.billFormData.debt) * 100;
-    const repayment = parseInt(viewState.billFormData.repay) * 100;
-    const rate = parseInt(viewState.billFormData.rate) / 100;
-    viewState.creditRepayment = calculateRepayments(debt, repayment, rate);
-    const creditChart = new Chart(document.getElementById('savings_chart'), {
+    const savings = parseInt(viewState.savingsFormData.savings) * 100;
+    const payment = parseInt(viewState.savingsFormData.payment) * 100;
+    const rate = parseInt(viewState.savingsFormData.rate) / 100;
+    viewState.savingsAmount = calculateSavingsInAYear(savings, payment, rate);
+    const savingsCart = new Chart(document.getElementById('savings_chart'), {
         type: 'line',
         data: {
-            labels: Object.keys(viewState.creditRepayment),
+            labels: Object.keys(viewState.savingsAmount),
             datasets: [{
-                label: 'Amount Remaining',
-                data: Object.values(viewState.creditRepayment).map(function(item) {
+                label: 'Amount Saved',
+                data: Object.values(viewState.savingsAmount).map(function(item) {
                     return parseInt(item) / 100;
                 })
             }],
@@ -52,25 +66,16 @@ function handleSavingsCalculation(event) {
     })
 }
 
-const viewState = {
-    billFormData: {
-        debt: 0,
-        rate: 0,
-        repay: 0
-    },
-    savingsFormData: {
-        savings: 0,
-        rate: 0,
-        payment: 0
-    },
-    creditRepayment: {}
-};
-
-function handleFormInputChanged(whichInput, event) {
+function handleDebtFormChanged(whichInput, event) {
     viewState.billFormData[whichInput] = event.target.value;
 }
 
-Vue.component('debt-input', creditCardInput);
+function handleSavingsFormChanged(whichInput, event) {
+    viewState.savingsFormData[whichInput] = event.target.value;
+}
+
+Vue.component('credit-card-input', creditCardInput);
+Vue.component('savings-input', savingsInput);
 
 const pageView = new Vue({
     el: '#root',
@@ -80,8 +85,21 @@ const pageView = new Vue({
         const h = this.$createElement;
         return (
             <div>
-                <debt-input debt={viewState.billFormData.debt} repay={viewState.billFormData.repay} rate={viewState.billFormData.rate} handle-form-submit={handleCreditCardDebtCalculation} handle-input-changed={handleFormInputChanged}/>
-                <canvas id="chart" width="400" height="400"/>
+                <credit-card-input
+                    debt={viewState.billFormData.debt}
+                    repay={viewState.billFormData.repay}
+                    rate={viewState.billFormData.rate}
+                    handle-form-submit={handleCreditCardDebtCalculation}
+                    handle-input-changed={handleDebtFormChanged}
+                />
+                <canvas id="credit_chart" width="400" height="400"/>
+                <savings-input
+                    initial-savings={viewState.savingsFormData.savings}
+                    saving={viewState.savingsFormData.payment}
+                    rate={viewState.savingsFormData.rate}
+                    handle-form-submit={handleSavingsCalculation}
+                    handle-input-changed={handleSavingsFormChanged}
+                />
                 <canvas id="savings_chart" width="400" height="400"/>
             </div>
         )
