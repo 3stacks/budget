01686ace4b8ad7196bdaa13accd132c0abf187d5
Luke Boyle <luke.boyle.au@gmail.com>
2017-03-01 12:13:35 +1100

    fix: incorrect calculation of monthly interest

--- src/js/utils/debt-utils.js
+++ src/js/utils/debt-utils.js
@@ -1,6 +1,6 @@
 export function calculateRepayments(debt, repay, interest, month = 1, valueSoFar = {}) {
     if (debt > 0) {
-        const monthlyInterest = (debt * interest) / 12;
+		const monthlyInterest = (((interest / 12) / 100) * debt) * 100;
         const newDebt = ((debt + monthlyInterest) - repay);
         return calculateRepayments(newDebt, repay, interest, month + 1, {...valueSoFar, [month]: debt});
     } else {
